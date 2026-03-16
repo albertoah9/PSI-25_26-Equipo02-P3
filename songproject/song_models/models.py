e79f0b7c1fe9e18ffb1c2c7c8d4b9622a363f223
albertoah9 <soyalbertoalonso@gmail.com>
2026-03-16 13:02:49 +0100

    Modelo Song incompleto

--- songproject/song_models/models.py
+++ songproject/song_models/models.py
@@ -4,6 +4,54 @@ from  django.contrib.auth.models import User
 class Song(models.Model):
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
+    
+    LANGUAJE_CHOICES = [
+        ('EN', 'English'),
+        ('ES', 'Spanish'),
+        ('FR', 'French'),
+        ('DE', 'German'),
+        ('IT', 'Italian'),
+        ('PT', 'Portuguese'),
+        ('JA', 'Japanese'),
+        ('ZH', 'Chinese'),
+    ]
+    
+    languaje = models.CharField(max_length=2, choices=LANGUAJE_CHOICES)
+    
+    # audio_file
+    
+    # lrc_file
+    
+    # background_image
+    
+    # created_at
+    
+    CATEGORY_CHOICES = [
+        ('POP', 'Pop'),
+        ('ROCK', 'Rock'),
+        ('JAZZ', 'Jazz'),
+        ('HIPHOP', 'Hip-Hop'),
+        ('CLASSICAL', 'Classical'),
+        ('REGGAE', 'Reggae'),
+        ('LATIN', 'Latin'),
+        ('KPOP', 'K-Pop'),
+        ('COUNTRY', 'Country'),
+        ('BLUES', 'Blues'),
+        ('FOLK', 'Folk'),
+        ('ELECTRONIC', 'Electronic'),
+        ('R&B', 'R&B'),
+        ('SOUL', 'Soul'),
+        ('METAL', 'Metal'),
+        ('PUNK', 'Punk'),
+        ('ALTERNATIVE', 'Alternative'),
+        ('INDIE', 'Indie'),
+        ('GOSPEL', 'Gospel'),
+        ('WORLD', 'World Music'),
+    ]
+    
+    # category
+    
+    # number_times_played
 
     def __str__(self):
         return f"{self.title} - {self.artist}"
