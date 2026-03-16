5b956a60227b443b64369692b8df78d71ae61ab5
Marcos Hernández <marcos.hernandezvidal@estudiante.uam.es>
2026-03-16 12:58:46 +0100

    modelo songUser

--- songproject/song_models/models.py
+++ songproject/song_models/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from  django.contrib.auth.models import User
 
 class Song(models.Model):
     title = models.CharField(max_length=255)
@@ -6,3 +7,15 @@ class Song(models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.artist}"
+
+class SongUser(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    played_at = models.DateTimeField(auto_now_add=True)
+    correct_guesses = models.IntegerField(default=0)
+    wrong_guesses = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.song} - {self.user}"
+
+    
