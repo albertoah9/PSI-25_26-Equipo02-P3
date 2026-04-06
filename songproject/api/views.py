c0ad04b5f73f8df78f6c978b76c2c20cfbd62826
albertoah9 <soyalbertoalonso@gmail.com>
2026-04-06 13:30:46 +0200

    Limpieza comentarios innecesarios y correcio de visualizacion

--- songproject/api/views.py
+++ songproject/api/views.py
@@ -9,7 +9,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 class SongViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Song.objects.all().order_by('id') # aqui he metido el order by porque si no el test da un warning de paginacion
+    queryset = Song.objects.all().order_by('id')
     serializer_class = SongSerializer
     permission_classes = [AllowAny]
     pagination_class = SongPagination
@@ -27,7 +27,6 @@ class SongViewSet(viewsets.ReadOnlyModelViewSet):
     @action(detail=False, methods=['get'])
     def top(self, request):
         n=request.query_params.get('n', 3)
-        # si falta la n hay que devolver 400 bad request (esto lo he metido nuevo)
         if n is None:
             return Response(
                 {'detail': 'Falta parametro n'},
@@ -37,7 +36,6 @@ class SongViewSet(viewsets.ReadOnlyModelViewSet):
         try:
             n = int(n)
         except ValueError:
-            #aqui he quitado lo de n = 3 porque los test fallan
             return Response(
                 {'detail': 'n tiene que ser int'},
                 status=400 
@@ -49,21 +47,20 @@ class SongViewSet(viewsets.ReadOnlyModelViewSet):
 
     @action(detail=False, methods=['get'])
     def search(self, request):
-        # antes estaba con ('title', '') con esa opcion si no se mandaba title se ponia como vacio y falla el test
         title = request.query_params.get('title') 
-        # si no hay titulo se devuelve 400
         if title is None:
             return Response(
                 {'detail': 'Parametro title invalido'},
                 status=400 
             )
+            
         songs = Song.objects.filter(title__icontains=title)
-        # si la cancion no existe se devuelve 400
         if not songs.exists():
             return Response(
                 {'detail': 'La cancion no existe'},
                 status=404
             )
+            
         serializer = self.get_serializer(songs, many=True)
         return Response(serializer.data)
     
@@ -71,11 +68,8 @@ class SongUserViewSet(viewsets.ModelViewSet):
     queryset = SongUser.objects.all()
     serializer_class = SongUserSerializer
     
-    # se obtienen solo los datos del usuario que hace la request y no de otros
     def get_queryset(self):
         return SongUser.objects.filter(user=self.request.user).order_by('id')
     
-    # Cuando haces post guarda solo el usuario que hace el post, de manera que no hay que meter el user
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
