f9cc2fea1e80197941fc8cb235715af3a1ec3bdd
Alberto <145713813+albertoah9@users.noreply.github.com>
2026-05-07 21:16:05 +0200

    Allow dynamic CORS origin configuration
    Add FRONTEND_URL to CORS_ALLOWED_ORIGINS if set

--- songproject/songproject/settings.py
+++ songproject/songproject/settings.py
@@ -148,3 +148,10 @@ CORS_ALLOWED_ORIGINS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+import os
+
+FRONTEND_URL = os.environ.get("FRONTEND_URL")
+
+if FRONTEND_URL:
+    CORS_ALLOWED_ORIGINS.append(FRONTEND_URL)
